628c859d5b9e25d934dad104d102dc5c5bb5c392
Admin <riverflow1928@yandex.ru>
2019-08-16 01:07:03 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import img1 from './images/img1.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"main"}>
+      <div className={"content-container"}>
+        <div className={"content"}>
+          <img className={"img1"} src={img1} />
+        </div>
+        {/* <img className={"img2"} src={img2} /> */}
+      </div>
     </div>
   );
 }
