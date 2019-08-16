5b25983a1b8283bd0cf33d682722e7f080d8c424
Konik Ivan <konik@skbkontur.ru>
2019-08-16 15:57:00 +0500

    Updates

---
+++ precache-manifest.64676220427d8107f4b35b3a3f58bb61.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5d2f5692943b65188977cfff2149e2",
+    "url": "/jewelry/index.html"
+  },
+  {
+    "revision": "deb87f221fc19cb6cada",
+    "url": "/jewelry/static/css/main.61058bd7.chunk.css"
+  },
+  {
+    "revision": "69b09955ef082810f5f4",
+    "url": "/jewelry/static/js/2.778a6255.chunk.js"
+  },
+  {
+    "revision": "deb87f221fc19cb6cada",
+    "url": "/jewelry/static/js/main.9f39db45.chunk.js"
+  },
+  {
+    "revision": "7be348575c6bd9a70c3d",
+    "url": "/jewelry/static/js/runtime~main.b6b47218.js"
+  },
+  {
+    "revision": "7214fe81e0d8bda2eff40d0ead83d9a2",
+    "url": "/jewelry/static/media/Intro-Book.7214fe81.ttf"
+  },
+  {
+    "revision": "0bcb7f7334a25828999f75d946c162c9",
+    "url": "/jewelry/static/media/Intro-Light.0bcb7f73.ttf"
+  },
+  {
+    "revision": "45ccb093c0f147299c2bf508f60632a6",
+    "url": "/jewelry/static/media/Lazurski.45ccb093.ttf"
+  },
+  {
+    "revision": "b58909c8feb65edb633208274af2e7f0",
+    "url": "/jewelry/static/media/diamond.b58909c8.svg"
+  },
+  {
+    "revision": "bd367f8cef2fa49cab3420833518839b",
+    "url": "/jewelry/static/media/img1.bd367f8c.svg"
+  },
+  {
+    "revision": "f078ff3e9bb4dccff649e0977f93c0e6",
+    "url": "/jewelry/static/media/img2.f078ff3e.png"
+  },
+  {
+    "revision": "e8aa332956878e818f018a45ff93a148",
+    "url": "/jewelry/static/media/slider1.e8aa3329.svg"
+  },
+  {
+    "revision": "0dd85a3e7830eafbaeefb586071918ad",
+    "url": "/jewelry/static/media/slider2.0dd85a3e.svg"
+  }
+]);
